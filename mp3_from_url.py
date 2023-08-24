ab847b99e1586c5b37aa22b0cdf0a2d7d18e2abf
Jonel Muncan <jonelm1901@gmail.com>
2023-08-24 16:34:18 -0400

    Add files via upload

--- mp3_from_url.py
+++ mp3_from_url.py
@@ -1,15 +1,28 @@
 from pytube import YouTube
-from os import path
+import os
 from pydub import AudioSegment
 import spleeter
 
 #NOTE: BPM detection uses WAV format, MP3 needs conversion
 def getMP3(url):
     try:
-        song = YouTube(url)
-        stream = song.streams.filter(only_audio=True).first()
-        stream.download(filename=f"{song.title}.mp3")
-        print("Song downloaded to MP3")
+        #song = YouTube(url)
+        #audio = song.streams.filter(only_audio=True).first()
+       # dest = "."
+        #out_file = audio.download(output_path=dest)
+        #base, ext = os.path.splitext(out_file)
+       # new_file = base + '.mp3'
+        #os.rename(out_file, new_file)
+       # print(song.title + " downloaded to MP3")
+        yt = YouTube(url)
+        video = yt.streams.filter(only_audio=True).first()
+        print("Enter the destination address (leave blank to save in current directory)")
+        destination = str(input(" ")) or '.'
+        out_file = video.download(output_path=destination)
+        base, ext = os.path.splitext(out_file)
+        new_file = base + '.mp3'
+        os.rename(out_file, new_file)
+        print(yt.title + " has been successfully downloaded.")
         return True
 
     except KeyError:
@@ -38,3 +51,14 @@ def splitter(filename): #not sure how to use this yet
 
     vocals.write("vocals.wav")
     instruments.write("instruments.wav")
+
+
+if __name__ == "__main__":
+    print("""Begin test
+    .
+    .
+    .
+    .
+    .
+    """)
+    getMP3("https://www.youtube.com/watch?v=psuRGfAaju4")
