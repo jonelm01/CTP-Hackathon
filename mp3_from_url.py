7c2e074f9c3120157ebdcdbbd7b5fbf7825e40eb
Jonel Muncan <jonelm1901@gmail.com>
2023-08-24 22:13:35 -0400

    Add files via upload

--- mp3_from_url.py
+++ mp3_from_url.py
@@ -1,4 +1,5 @@
 from pytube import YouTube
+from pythumb import Thumbnail
 import os
 from pydub import AudioSegment
 import spleeter
@@ -6,21 +7,28 @@ from IPython.display import Audio
 
 #NOTE: BPM detection uses WAV format, MP3 needs conversion
 #Fetch/download .mp3 of YouTube video at provided URL
-def getMP3(url):
+def getMP3andThumbnail(url):
     try:
         yt = YouTube(url)
         video = yt.streams.filter(only_audio=True).first()
         destination = '.'
         out_file = video.download(output_path=destination)
         base, ext = os.path.splitext(out_file)
-        new_file = base + '.mp3'
+        #base.replace(" ", "_")
+        name = base.replace(" ", "-")
+        print(name)
+        new_file = name + '.mp3'
         os.rename(out_file, new_file)
         print(yt.title + " has been successfully downloaded.")
-        return True
+
+        img = Thumbnail(url)
+        img.fetch()
+        img.save('.', name)
+
+        return name, yt.title
 
     except KeyError:
         print("Unable to retrieve stream")
-        return False
         #Bad url
 
 
@@ -40,22 +48,22 @@ def MP3toWav(MP3_filename): #name of file without .mp3 extension
 
 
 #Splits "filename" song into filename_accompaniment.wav and filename_vocals.wav
-def splitter(filename): 
-    os.system("spleeter separate -o " + "split_songs/ " + filename + ".mp3")
-    os.system("mv accompaniment.wav " + filename + "_accompaniment.wav")
-    os.system("mv vocals.wav " + filename + "_vocals.wav")
+def splitter(path, file): 
+    file = file.replace(" ", "-")
+    print(path)
+    print(file)
+    os.system("spleeter separate -o " + "split_songs/ " + path + ".mp3")
+    #os.system("cd split_songs")
+    os.system("mv ./split_songs/" + file + "/" + "accompaniment.wav " + "./split_songs/" + file + "/" + file + "_accompaniment.wav")
+    os.system("mv ./split_songs/" + file + "/" + "vocals.wav " + "./split_songs/" + file + "/" + file + "_vocals.wav")
+    #os.system("mv vocals.wav " + filename + "_vocals.wav")
 #######USE CLI -> spleeter separate -o direct/ filename.mp3
 
 
 #UNTESTED
-#Clear songs in directory on refresh, opem, close, or manual refresh button
+#Clear songs in directory on refresh, open, close, or manual refresh button
 def clearSongs():
     os.system("rm -r /split_songs")
-
-
-def merger(song1, song2):
-    #merge 
-    return 1
 
 
 if __name__ == "__main__":
@@ -67,7 +75,8 @@ if __name__ == "__main__":
     .
     """)
 
-    #splitter("Fireflies")
+    name, base = getMP3andThumbnail("https://www.youtube.com/watch?v=NUUpIuspG5E")
+    splitter(name, base)
 
     
     
