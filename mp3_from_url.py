035805b71695ac0d2a14ba8c776d9c2766bcd1bb
Jonel Muncan <jonelm1901@gmail.com>
2023-08-24 22:49:03 -0400

    All functional, not fully modularized
    All functions work as needed in their own scope but are not properly/fully modularized to be integrated into webapp.

--- mp3_from_url.py
+++ mp3_from_url.py
@@ -4,8 +4,10 @@ import os
 from pydub import AudioSegment
 import spleeter
 from IPython.display import Audio
+import librosa
 
 #NOTE: BPM detection uses WAV format, MP3 needs conversion
+
 #Fetch/download .mp3 of YouTube video at provided URL
 def getMP3andThumbnail(url):
     try:
@@ -25,7 +27,7 @@ def getMP3andThumbnail(url):
         img.fetch()
         img.save('.', name)
 
-        return name, yt.title
+        return name, yt.title #path and song name
 
     except KeyError:
         print("Unable to retrieve stream")
@@ -47,7 +49,7 @@ def MP3toWav(MP3_filename): #name of file without .mp3 extension
         return False
 
 
-#Splits "filename" song into filename_accompaniment.wav and filename_vocals.wav
+#Splits "file" song at "path" into file_accompaniment.wav and file_vocals.wav
 def splitter(path, file): 
     file = file.replace(" ", "-")
     print(path)
@@ -66,6 +68,21 @@ def clearSongs():
     os.system("rm -r /split_songs")
 
 
+#Estimate BPM of "filename" song
+#More accurate than bpm_detection.py, but still noticably off
+def BPMest(filename):
+    audio_file = librosa.load(filename)
+    y, sr = audio_file
+    bpm, frames = librosa.beat.beat_track(y=y, sr=sr)
+    print("Estimated bpm: " + bpm)
+    
+
+#Pass the song whose speed will change, its BPM, and the BPM of the dependent song
+def playbackSpeed(filename, mainBPM, modBPM):
+    audio = AudioSegment.from_file(filename, format="wav")
+    mod = float(mainBPM / modBPM)
+    audio.speedup(playbackSpeed=mod)
+
 if __name__ == "__main__":
     print("""Begin test
     .
@@ -75,8 +92,9 @@ if __name__ == "__main__":
     .
     """)
 
-    name, base = getMP3andThumbnail("https://www.youtube.com/watch?v=NUUpIuspG5E")
-    splitter(name, base)
+    #name, base = getMP3andThumbnail("https://www.youtube.com/watch?v=NUUpIuspG5E")
+    #splitter(name, base)
+    BPMest("Yellowman_accompaniment.wav")
 
     
     
